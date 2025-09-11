d8358cc0ef152bb732b9f20f8d8d80cbe154f645
Douglas Soares <douglassoares.cinema@gmail.com>
2025-09-11 14:36:09 -0300

    melhorando galeria com lightbox

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ let indice = 0;
 const slides = document.getElementById('slides');
 const totalSlides = slides.children.length;
 const visiveis = 1;
+const fotos = document.querySelectorAll('.slide img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.lightbox .close');
 
 function mudaSlide(direcao) {
     indice = indice + direcao * visiveis;
@@ -11,3 +15,21 @@ function mudaSlide(direcao) {
 
     slides.style.transform = `translateX(${-indice * (100 / visiveis)}%)`;
 }
+
+fotos.forEach(foto => {
+    foto.addEventListener('click', () => {
+        lightbox.style.display = 'block';
+        lightboxImg.src = foto.src;
+        lightboxImg.alt = foto.alt;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = 'none';
+    }
+});
